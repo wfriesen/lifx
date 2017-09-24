303d7be871ceb5e4b63c2cec2f606fa20a78a366
William Friesen <wfriesen@gmail.com>
2017-09-24 15:42:09 +1000

    add basic functionality. list lights and toggle on/off

--- src/App.js
+++ src/App.js
@@ -1,20 +1,63 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    const token = window.localStorage.getItem('token');
+    this.state = {
+      lights: [],
+      token: token ? token : '',
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <label htmlFor="token">Access Token:</label>
+        <input
+          id="token"
+          type="text"
+          value={this.state.token}
+          onChange={e => this.onChangeToken(e.target.value)}
+        />
+        <button onClick={this.listlights.bind(this)}>Go</button>
+        {this.state.lights.map(light => (
+          <p key="{light.id}" onClick={() => this.toggleLight(light.id)}>
+            {light.label}
+          </p>
+        ))}
       </div>
     );
+  }
+
+  onChangeToken(token) {
+    window.localStorage.setItem('token', token);
+    this.setState({token});
+  }
+
+  getHeaders() {
+    return {
+      Authorization: 'Bearer ' + this.state.token,
+    };
+  }
+
+  async listlights(e) {
+    const lights = await fetch('https://api.lifx.com/v1/lights/all', {
+      method: 'get',
+      headers: this.getHeaders(),
+    }).then(function(res) {
+      return res.json();
+    });
+
+    this.setState({lights});
+  }
+
+  toggleLight(id) {
+    fetch('https://api.lifx.com/v1/lights/' + id + '/toggle', {
+      method: 'post',
+      headers: this.getHeaders(),
+    });
   }
 }
 
